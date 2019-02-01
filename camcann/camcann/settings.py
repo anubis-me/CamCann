fc796a830960296fd0bc4ff2dcd855e0a011ed43
abhishek2507 <abhibuddy1234@gmail.com>
2019-02-01 22:48:42 +0530

    Models Created for database

--- camcann/camcann/settings.py
+++ camcann/camcann/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'collector.apps.CollectorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- camcann/collector/models.py
+++ camcann/collector/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+import datetime
+class x(models.Model):
+    camera = models.CharField(max_length=150)
+    Gender = models.CharField(max_length=20)
+    Age = models.IntegerField(max_length=5)
