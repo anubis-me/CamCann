86e5a4d9ab4b3c9c9c4a1547621859fadbd668c2
DashwoodIce9 <dashwoodice9@TheMachine.Ubuntu>
2019-02-01 20:53:34 +0530

    Initilization

---
+++ camcann/collector/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import url
+urlpatterns = [
+    url('^$', views.index, name='index'),
+]
---
+++ camcann/collector/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("<h1>This is the collector app for data collection/<h1>")
