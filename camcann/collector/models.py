56ace06964d04aeecf00ede088cd2e890f57126c
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2019-02-03 22:43:51 +0530

    Models Update

--- camcann/collector/models.py
+++ camcann/collector/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 import datetime
+import os
+
+def get_image_path(instance, filename):
+    return os.path.join('images', str(instance.id), filename)
+
 class x(models.Model):
     camera = models.CharField(max_length=150)
     Gender = models.CharField(max_length=20)
-    Age = models.IntegerField(max_length=5)
+    Age = models.IntegerField()
+    Image = models.ImageField(upload_to = get_image_path)
+    Timestamp = models.DateTimeField()
+    yaw = models.IntegerField()
+    pitch = models.IntegerField()
+    roll = models.IntegerField()
