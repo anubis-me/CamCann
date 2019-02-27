991d7de460738b69ec97ef7ab8803d8055e29adc
abhishek2507 <abhibuddy1234@gmail.com>
2019-02-27 22:20:23 +0530

    Database and routes ready

--- camcann/collector/models.py
+++ camcann/collector/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 
 class Data(models.Model):
-    camera = models.CharField(max_length=150)
-    Gender = models.CharField(max_length=20)
-    Age = models.IntegerField(max_length=5)
-    image_name = models.CharField(max_length=150, default="images1")
-    Image = "/images/" + str(image_name)
-    head_yaw = models.FloatField(max_length=200, default=0)
-    head_pitch = models.FloatField(max_length=200, default=0)
-    head_roll = models.FloatField(max_length=200, default=0)
+    image_name = models.CharField(max_length=150, default="")
+    head_roll = models.CharField(max_length=150, default="")
+    head_yaw = models.CharField(max_length=150, default="")
+    camera = models.CharField(max_length=150, default="")
+    head_pitch = models.CharField(max_length=150, default="")
+    Frame_Shape = models.CharField(max_length=150, default= [0,0,0])
+    Age = models.IntegerField(default="NA")
+    Image = models.CharField(max_length=150, default="images1")
+    Gender = models.CharField(max_length=20, default="NA")
+    Location = models.CharField(max_length=300, default="images/")
     Timestamp = models.CharField(max_length=250, default="000000")
 
     def __str__(self):
-        return self.camera + '-' + self.Gender + '-' + str(self.Age)
+        return self.Image + '-' + self.Gender + '-' + str(self.Age)
